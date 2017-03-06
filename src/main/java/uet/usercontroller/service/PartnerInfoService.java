6980c3df6aef34dc16279bcbeb85e59f9870d2b7
nhkhanh068 <nhkhanh068@gmail.com>
2017-03-06 16:03:11 +0700

    tra ve them partnerId khi get logo

--- src/main/java/uet/usercontroller/service/PartnerInfoService.java
+++ src/main/java/uet/usercontroller/service/PartnerInfoService.java
@@ -254,6 +254,7 @@ public class PartnerInfoService {
             lPartnerInfo.put("partnerName", partnerName);
             lPartnerInfo.put("logo", logo);
             lPartnerInfo.put("partnerInfoId",String.valueOf(partnerInfoId));
+            lPartnerInfo.put("partnerId",String.valueOf(partner.getId()));
             listPartnerInfo.add(lPartnerInfo);
         }
         return listPartnerInfo;
